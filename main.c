e185b2424b0d5626fcd9691637e428acdc50278c
Lucas Barbosa Da Rocha <lb0060928@gmail.com>
2024-11-14 11:40:25 -0300

    Update main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include  <string.h>
+#define MAXN 100000
+
+typedef struct armarzenamento{
+    char nome[50];
+    char codigo[20];
+    int quantidade;
+    char categoria[20];
+    char locacao[50];
+    char estado[20];
+
+
+} Item;
+
+Item BaseDados[MAXN];
+
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+return 0;}
