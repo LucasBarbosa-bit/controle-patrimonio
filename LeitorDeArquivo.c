2b64a60a377178d750a584ac7478d7dec6d3a8b3
vbaumel2 <vbm4@poli.br>
2024-11-14 12:07:04 -0300

    Update LeitorDeArquivo.c

--- LeitorDeArquivo.c
+++ LeitorDeArquivo.c
@@ -2,25 +2,43 @@
 #include<string.h>
 #include<stdbool.h>
 
-#define MAXN 1000
+#define MAXN 5000 // tamanho maximo de uma linha
+#define MAXE 20 //  numero maximo de elementos em cada linha
+
+
+char row[MAXN];
+char *token;
+int N_Ent = 0;
+char elementos[MAXE][MAXN]; // elementos da primeira linha (nome dos elementos)
 
 int main(){
 
     FILE *fp;
-    char row[MAXN];
-    char *token;
 
-    fp = fopen("teste1.csv","r");
+    fp = fopen("lista.csv","r");
 
+
+    fgets(row, MAXN, fp);
+
+    token = strtok(row , ",");
+    while(token != NULL)
+    {
+        strcpy(elementos[N_Ent], token);
+        N_Ent++;
+
+        token = strtok(NULL, ",");
+    }
 
 
     while(fgets(row, MAXN, fp)) {
 
             token = strtok(row, ",");
 
+            int cnt = 0;
             while(token != NULL)
            {
-              printf("%s\n", token);
+              printf("%s: %s", elementos[cnt++] ,token);
+              printf("\n");
               token = strtok(NULL, ",");
            }
     }
